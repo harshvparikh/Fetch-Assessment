fb316f8e8f15df7ec8a2e98625b5896215de308a
Harsh Parikh <harshparikh016@gmail.com>
2024-06-22 19:33:20 -0500

    Update users.sql

--- Data Modeling/staging_tables/users.sql
+++ Data Modeling/staging_tables/users.sql
@@ -1,12 +1,12 @@
 CREATE TEMP TABLE users_raw_json (data jsonb);
 
-/* psql -h hp -U harshparikh -d fetch_db -c "\COPY raw_json(data) FROM 'sql_scripts/users.json'"
+/* psql -h hp -U harshparikh -d fetch_db -c "\COPY users_raw_json(data) FROM 'sql_scripts/users.json'"
 */ 
 
 CREATE TABLE users AS
 WITH json_data AS (
     SELECT jsonb_array_elements(data) AS json_obj
-    FROM raw_json
+    FROM users_raw_json
 )
 SELECT 
     (json_obj -> '_id' ->> '$oid') AS id,
@@ -16,7 +16,7 @@ SELECT
     (json_obj ->> 'role')::text AS role,
     (json_obj ->> 'signUpSource')::text AS sign_up_source,
     (json_obj ->> 'state')::text AS state
-FROM json_data;
+FROM users_raw_json;
 
 ALTER TABLE users ADD PRIMARY KEY (id);
 
