c7121d7e317142ff4c7f7862790821517a561a21
Harsh Parikh <harshparikh016@gmail.com>
2024-06-22 19:35:49 -0500

    Create item_receipts.sql

---
+++ Data Modeling/staging_tables/item_receipts.sql
@@ -0,0 +1,42 @@
+
+CREATE TEMP TABLE item_receipts_raw_json (data jsonb);
+
+/* To be run in shell:  psql -h hp -U harshparikh -d fetch_db -c "\COPY item_receipts_raw_json(data) FROM 'sql_scripts/item_receipts.json'" */
+
+CREATE TABLE item_receipts AS
+WITH json_data AS (
+    SELECT jsonb_array_elements(data) AS json_obj
+    FROM item_receipts_raw_json
+)
+SELECT 
+    receipt_oid,
+    item->>'barcode' AS barcode,
+    item->>'brandCode' AS brand_code,
+    item->>'competitorRewardsGroup' AS competitor_rewards_group,
+    item->>'description' AS description,
+    (item->>'discountedItemPrice')::DECIMAL AS discounted_item_price,
+    (item->>'finalPrice')::DECIMAL AS final_price,
+    item->>'itemNumber' AS item_number,
+    (item->>'itemPrice')::DECIMAL AS item_price,
+    (item->>'needsFetchReview')::BOOLEAN AS needs_fetch_review,
+    item->>'needsFetchReviewReason' AS needs_fetch_review_reason,
+    (item->>'originalFinalPrice')::DECIMAL AS original_final_price,
+    item->>'originalMetaBriteBarcode' AS original_meta_brite_barcode,
+    item->>'originalMetaBriteDescription' AS original_meta_brite_description,
+    (item->>'originalMetaBriteItemPrice')::DECIMAL AS original_meta_brite_item_price,
+    (item->>'originalMetaBriteQuantityPurchased')::INT AS original_meta_brite_quantity_purchased,
+    item->>'partnerItemId' AS partner_item_id,
+    (item->>'pointsEarned')::DECIMAL AS points_earned,
+    item->>'pointsNotAwardedReason' AS points_not_awarded_reason,
+    item->>'pointsPayerId' AS points_payer_id,
+    (item->>'preventTargetGapPoints')::BOOLEAN AS prevent_target_gap_points,
+    (item->>'quantityPurchased')::INT AS quantity_purchased,
+    item->>'rewardsGroup' AS rewards_group,
+    item->>'rewardsProductPartnerId' AS rewards_product_partner_id,
+    (item->>'targetPrice')::DECIMAL AS target_price,
+    item->>'userFlaggedBarcode' AS user_flagged_barcode,
+    item->>'userFlaggedDescription' AS user_flagged_description,
+    (item->>'userFlaggedNewItem')::BOOLEAN AS user_flagged_new_item,
+    (item->>'userFlaggedPrice')::DECIMAL AS user_flagged_price,
+    (item->>'userFlaggedQuantity')::INT AS user_flagged_quantity
+FROM item_receipts_raw_json;
